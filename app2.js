8947b536bfa0456074afc70b3a5fc11431925b37
Sean Leif Hellier <sean@newtropolis.com>
2022-02-06 10:02:56 -0500

    finally got it working thanks to you Tony H o

--- app2.js
+++ app2.js
@@ -25,7 +25,8 @@ var numWins = document.getElementById("numWins");
 var numLosses = document.getElementById("numLosses");
 
 var gameActive = true;
-var guessesLeft = 10;
+var guessesLeft = 0;
+var losses = 0;
 var answer = "";
 var foundMatch = false;
 
@@ -34,37 +35,54 @@ var musicalActs = ["apollo100", "beethoven", "mozart"];
 var startGame = function (guess) {
 	var choice = Math.floor(Math.random() * musicalActs.length);
 	var answer = musicalActs[choice];
+	var blanks = [];
 	console.log(answer, "computer guess");
-	checkGuess(answer);
+	// guessesLeft = 10;
+	// document.getElementById("guessesLeft").innerHTML = "guessesLeft";
 
 	var letters = Array.from(answer);
 	console.log("letters", letters);
 	var blanksNum = answer.length;
 	// console.log(blanksNum, "blanksNum");
-	displayLetters();
+	createBlanks();
 
-	function displayLetters() {
-		var blanks = [];
+	function createBlanks() {
 		for (let i = 0; i < answer.length; i++) {
 			blanks.push("_");
 			guessWord.innerHTML = blanks.join(" ");
 			console.log(blanks, "blanks");
 		}
+
+		//if correctLetter is in the answer, then we replce the blank index with this letter
 	}
 
-	function checkGuess(guess, blanks) {
+	function checkGuess(guess) {
+		guessesLeft = 10;
 		console.log(guess, "checkGuess");
 		console.log(answer, "answer in checkGuess");
 		var foundMatch = false;
+		let indexes = [];
+
 		for (var i = 0; i < answer.length; i++) {
 			console.log(answer.length, "answer.length");
 			if (guess === answer[i]) {
-				foundMatch = true;
-				blanks[i] = guess;
-
-				displayLetters();
+				indexes.push(i);
 			}
 		}
+
+		//if there are matches
+		if (indexes.length > 0) {
+			//foundMatch = true;
+			for (let q = 0; q < indexes.length; q++) {
+				blanks[indexes[q]] = guess;
+			}
+		}
+		guessWord.innerHTML = blanks.join(" ");
+	}
+	var str = "scissors";
+	var indices = [];
+	for (var i = 0; i < str.length; i++) {
+		if (str[i] === "s") indices.push(i);
 	}
 
 	window.onload = function () {
